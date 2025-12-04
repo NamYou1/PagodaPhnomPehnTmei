0182a059738e2b2dbfd2d15b653bec0f23d4bc4a
NamYou1 <namyou854@gmail.com>
2025-12-04 20:28:20 +0700

    add more photo

--- src/components/Pages/data.js
+++ src/components/Pages/data.js
@@ -13,6 +13,22 @@ import child12 from "../../assets/ImagePagoda/kralkrong/child12.jpg";
 import child13 from "../../assets/ImagePagoda/kralkrong/child13.jpg";
 import child14 from "../../assets/ImagePagoda/kralkrong/child14.jpg";
 
+import {
+    Kathina_Festival_main,
+    Kathina_Festival1,
+    Kathina_Festival2,
+    Kathina_Festival3,
+    Kathina_Festival4,
+    Kathina_Festival5,
+    Kathina_Festival6,
+    Kathina_Festival7,
+    Kathina_Festival8,
+    Kathina_Festival9,
+    Kathina_Festival10,
+    Kathina_Festival11,
+    Kathina_Festival12,
+} from "../../assets/Kathina_Festival/index"
+
 
 export const initialData = [
     {
@@ -43,111 +59,32 @@ export const initialData = [
         ]
     },
     {
-        id: 1,
-        title: "Mountain Adventure",
-        titleKm: "កឋិនទាន",
+        id: 2,
+        title: "Kathina Festival",
+        titleKm: "ពិធីចម្រើនព្រះបរិត បុណ្យកឋិន",
         description:
             "Enjoy the breathtaking views and the thrill of climbing the majestic mountains.",
         descriptionKm:
             "រីករាយនឹងទិដ្ឋភាពដ៏អស្ចារ្យ និងភាពរំភើបក្នុងការឡើងលើភ្នំដ៏ស្ម័គ្រចឹត។",
-        imgUrl: main,
+        imgUrl: Kathina_Festival1,
         vdourl: "https://www.facebook.com/share/v/17SAmw2aMU/",
-        year: 2024,
+        year: 2025,
         Children: [
-            // { id: 1, image: child1 },
-            // { id: 2, image: child2 },
-            { id: 3, image: child3 },
-            { id: 4, image: child4 },
-            { id: 5, image: child5 },
-            { id: 6, image: child6 },
-            // { id: 7, image: child7 },
-            { id: 8, image: child8 },
-            { id: 9, image: child9 },
-            // { id: 10, image: child10 },
-            { id: 8, image: child12 },
-            { id: 8, image: child13 },
-            { id: 8, image: child14 },
+
+            { id: 1, image: Kathina_Festival_main },
+            { id: 2, image: Kathina_Festival2 },
+            { id: 3, image: Kathina_Festival3 },
+            { id: 4, image: Kathina_Festival4 },
+            { id: 5, image: Kathina_Festival5 },
+            { id: 6, image: Kathina_Festival6 },
+            { id: 7, image: Kathina_Festival7 },
+            { id: 8, image: Kathina_Festival8 },
+            { id: 9, image: Kathina_Festival9 },
+            { id: 10, image: Kathina_Festival10 },
+            { id: 8, image: Kathina_Festival11 },
+            { id: 8, image: Kathina_Festival12 },
         ]
     },
-    {
-        id: 1,
-        title: "Mountain Adventure",
-        titleKm: "កឋិនទាន",
-        description:
-            "Enjoy the breathtaking views and the thrill of climbing the majestic mountains.",
-        descriptionKm:
-            "រីករាយនឹងទិដ្ឋភាពដ៏អស្ចារ្យ និងភាពរំភើបក្នុងការឡើងលើភ្នំដ៏ស្ម័គ្រចឹត។",
-        imgUrl: main,
-        vdourl: "https://www.facebook.com/share/v/17SAmw2aMU/",
-        year: 2024,
-        Children: [
-            // { id: 1, image: child1 },
-            // { id: 2, image: child2 },
-            { id: 3, image: child3 },
-            { id: 4, image: child4 },
-            { id: 5, image: child5 },
-            { id: 6, image: child6 },
-            // { id: 7, image: child7 },
-            { id: 8, image: child8 },
-            { id: 9, image: child9 },
-            // { id: 10, image: child10 },
-            { id: 8, image: child12 },
-            { id: 8, image: child13 },
-            { id: 8, image: child14 },
-        ]
-    },
-    {
-        id: 1,
-        title: "Mountain Adventure",
-        titleKm: "កឋិនទាន",
-        description:
-            "Enjoy the breathtaking views and the thrill of climbing the majestic mountains.",
-        descriptionKm:
-            "រីករាយនឹងទិដ្ឋភាពដ៏អស្ចារ្យ និងភាពរំភើបក្នុងការឡើងលើភ្នំដ៏ស្ម័គ្រចឹត។",
-        imgUrl: main,
-        vdourl: "https://www.facebook.com/share/v/17SAmw2aMU/",
-        year: 2024,
-        Children: [
-            // { id: 1, image: child1 },
-            // { id: 2, image: child2 },
-            { id: 3, image: child3 },
-            { id: 4, image: child4 },
-            { id: 5, image: child5 },
-            { id: 6, image: child6 },
-            // { id: 7, image: child7 },
-            { id: 8, image: child8 },
-            { id: 9, image: child9 },
-            // { id: 10, image: child10 },
-            { id: 8, image: child12 },
-            { id: 8, image: child13 },
-            { id: 8, image: child14 },
-        ]
-    },
-    {
-        id: 1,
-        title: "Mountain Adventure",
-        titleKm: "កឋិនទាន",
-        description:
-            "Enjoy the breathtaking views and the thrill of climbing the majestic mountains.",
-        descriptionKm:
-            "រីករាយនឹងទិដ្ឋភាពដ៏អស្ចារ្យ និងភាពរំភើបក្នុងការឡើងលើភ្នំដ៏ស្ម័គ្រចឹត។",
-        imgUrl: main,
-        vdourl: "https://www.facebook.com/share/v/17SAmw2aMU/",
-        year: 2024,
-        Children: [
-            // { id: 1, image: child1 },
-            // { id: 2, image: child2 },
-            { id: 3, image: child3 },
-            { id: 4, image: child4 },
-            { id: 5, image: child5 },
-            { id: 6, image: child6 },
-            // { id: 7, image: child7 },
-            { id: 8, image: child8 },
-            { id: 9, image: child9 },
-            // { id: 10, image: child10 },
-            { id: 8, image: child12 },
-            { id: 8, image: child13 },
-            { id: 8, image: child14 },
-        ]
-    },
+
+
 ];
