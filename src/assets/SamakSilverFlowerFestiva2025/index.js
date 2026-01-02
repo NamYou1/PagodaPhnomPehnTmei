43dcbcb3b1b759cc13e8086cd1e8a49f25cbcc24
NamYou1 <namyou854@gmail.com>
2026-01-02 18:21:49 +0700

    add more card

--- src/assets/SamakSilverFlowerFestiva2025/index.js
+++ src/assets/SamakSilverFlowerFestiva2025/index.js
@@ -1,4 +1,4 @@
-const modules = import.meta.glob('./SamakSilverFlowerFestival[0-9]*.jpg', { eager: true });
+const modules = import.meta.glob('./SamakSilverFlower[0-9]*.jpg', { eager: true });
 
 const SamakSilverFlowerFestivalPhoto = Object.values(modules).map(module => module.default);
 
